8a9835fc383bd9f0d8233b463fbad5c048164a76
jhorkman bernal <jhorber95@gmail.com>
2019-09-30 11:14:00 -0500

    refactor code

--- lib/src/providers/pelicula_provider.dart
+++ lib/src/providers/pelicula_provider.dart
@@ -8,15 +8,25 @@ class PeliculaProvider {
   String _url = 'api.themoviedb.org';
   String _lang = 'en-US';
 
-  Future<List<Pelicula>> getEnCines() async {
-    final url = Uri.https(
-        _url, '3/movie/now_playing', {'api_key': _apiKey, 'language': _lang});
-
+  Future<List<Pelicula>> _makeResponse(Uri url) async {
     final response = await http.get(url);
     final decodedData = json.decode(response.body);
 
     final peliculas = new Peliculas.fromJsonList(decodedData['results']);
 
     return peliculas.items;
+  }
+
+  Future<List<Pelicula>> getEnCines() async {
+    final url = Uri.https(
+        _url, '3/movie/now_playing', {'api_key': _apiKey, 'language': _lang});
+
+    return _makeResponse(url);
+  }
+
+  Future<List<Pelicula>> getPopular() async {
+    final url = Uri.https(
+        _url, '3/movie/popular', {'api_key': _apiKey, 'language': _lang});
+    return _makeResponse(url);
   }
 }
